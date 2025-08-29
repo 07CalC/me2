7f97029373578579c4dc147dd9980a3283e3c9eb
calc <maheshwarivinayak90@gmail.com>
2025-08-29 23:23:06 +0530

    new blog: web-is-just-text-over-tcp

---
+++ src/app/bytes/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next';
+
+
+
+
+export const metadata: Metadata = {
+  title: 'Bytes | Vinayak',
+  description: 'Byte sized information you never knew you needed.',
+
+};
